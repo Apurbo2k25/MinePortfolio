b4f6871fc5f7022d48b0c07ba5c546e6845b9ddb
Apurbo2025 <itschakrabarty001@gmail.com>
2025-07-03 20:41:56 +0530

    update life.js

---
+++ life.js
@@ -0,0 +1,5 @@
+// Show alert when the portfolio loads
+window.addEventListener("DOMContentLoaded", function () {
+    alert("🚧 My portfolio is under construction and will be updated soon. Thank you for your patience!");
+  });
+  
